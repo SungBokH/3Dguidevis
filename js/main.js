c310ee2df1a38a179623326e86600b2257ee7b93
SungBokH <sbshin90@umd.edu>
2020-08-07 03:45:48 -0400

    two cameras in one scene

--- js/main.js
+++ js/main.js
@@ -8,8 +8,9 @@ animate();
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-    camera.position.z = 1;
+    camera = new THREE.PerspectiveCamera( /* view angle*/45, /* aspect*/ window.innerWidth / window.innerHeight, /* near*/ 0.01,/* far*/ 10 );
+    camera.position.set(500,2000,550);
+    //camera.position.z = 1;
 
     scene = new THREE.Scene();
 
@@ -22,6 +23,19 @@ function init() {
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
+
+    // orthographic cameras
+	mapCamera = new THREE.OrthographicCamera(
+        window.innerWidth / -2,		// Left
+        window.innerWidth / 2,		// Right
+        window.innerHeight / 2,		// Top
+        window.innerHeight / -2,	// Bottom
+        -5000,            			// Near 
+        10000 );           			// Far 
+        mapCamera.up = new THREE.Vector3(0,0,-1);
+        mapCamera.lookAt( new THREE.Vector3(0,-1,0) );
+        scene.add(mapCamera);
+
 
 }
 
